9c5cf2fb6531ecfa6da79ddf6c3cd35f795d93b3
somsomi3 <chfhddlfhqls@naver.com>
2024-11-19 01:46:44 +0900

    accounts 추가(serializer, model)

--- back/moA/settings.py
+++ back/moA/settings.py
@@ -152,6 +152,10 @@ MEDIA_URL = 'media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_ROOT = BASE_DIR / 'media'
 
+# STATICFILES_DIRS= [
+#     BASE_DIR / 'static'
+# ]
+
 import os
 import environ 
 
