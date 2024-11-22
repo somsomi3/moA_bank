15801ffffc9b9ba64ed5fcbb601490f547b54a97
wongyeom1998 <dnjsrua0514@gmail.com>
2024-11-22 23:52:28 +0900

    커뮤니티개선

--- back/cards/urls.py
+++ back/cards/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
 from django.urls import path
-from .views import CardImageAPIView
+# from .views import CardImageAPIView
 # # Router 설정 (ViewSet용)
 # router = DefaultRouter()
 # router.register(r'cards', views.CardViewSet, basename='card')
@@ -16,7 +16,7 @@ urlpatterns = [
     # path('cards/list-create/', views.card_list_create, name='card-list-create'),
     # path('cards/<int:card_id>/', views.card_detail, name='card-detail'),
     # path('card-designs/list-create/', views.card_design_list_create, name='card-design-list-create'),
-    path('card-designs/<int:card_design_id>/', views.card_design_detail, name='card-design-detail'),
-    path('card-designs/generate_card/', views.generate_card, name='card-design-detail'),
-    path("generate-image/", CardImageAPIView.as_view(), name="generate_image"),
+    # path('card-designs/<int:card_design_id>/', views.card_design_detail, name='card-design-detail'),
+    # path('card-designs/generate_card/', views.generate_card, name='card-design-detail'),
+    # path("generate-image/", CardImageAPIView.as_view(), name="generate_image"),
 ]
